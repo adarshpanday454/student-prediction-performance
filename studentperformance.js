32f4d3aba3f7798b18ac400a948ced99a59e2ca8
adarshpanday454 <adrashpandey454@gmail.com>
2024-11-13 12:39:10 +0530

    Update studentperformance.js

--- studentperformance.js
+++ studentperformance.js
@@ -13,13 +13,4 @@ function predictPerformance() {
         prediction = "Low probability of good performance";
     }
 
-    // Display the result
-    document.getElementById("predictionResult").innerText = prediction;
-     var typed2 = new Typed('#typed2', {
-    strings: ['Some <i>strings</i> with', 'Some <strong>HTML</strong>', 'Chars &times; &copy;'],
-    typeSpeed: 0,
-    backSpeed: 0,
-    fadeOut: true,
-    loop: true
-  });
 }
