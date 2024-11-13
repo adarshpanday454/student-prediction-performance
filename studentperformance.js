2567166eeb6481772a8c606a933ce8e7789bed47
adarshpanday454 <adrashpandey454@gmail.com>
2024-11-13 12:38:44 +0530

    Update studentperformance.js

--- studentperformance.js
+++ studentperformance.js
@@ -15,4 +15,11 @@ function predictPerformance() {
 
     // Display the result
     document.getElementById("predictionResult").innerText = prediction;
+     var typed2 = new Typed('#typed2', {
+    strings: ['Some <i>strings</i> with', 'Some <strong>HTML</strong>', 'Chars &times; &copy;'],
+    typeSpeed: 0,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: true
+  });
 }
